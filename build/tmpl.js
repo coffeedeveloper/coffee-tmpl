5820b2b64a40739973fd69e40a57e4cbe306db2f
CoffeeDeveloper <coffeedeveloper@gmail.com>
2016-01-19 11:44:26 +0800

    完成单元测试及相关文档和代码优化

--- build/tmpl.js
+++ build/tmpl.js
@@ -1,135 +1,78 @@
 (function (global, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define(['exports', 'module'], factory);
-  } else if (typeof exports !== 'undefined' && typeof module !== 'undefined') {
-    factory(exports, module);
+  if (typeof define === "function" && define.amd) {
+    define(['exports'], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(exports);
   } else {
     var mod = {
       exports: {}
     };
-    factory(mod.exports, mod);
+    factory(mod.exports);
     global.tmpl = mod.exports;
   }
-})(this, function (exports, module) {
+})(this, function (exports) {
   'use strict';
 
-  var global = window;
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
 
-  var support = {
-    localStorage: 'localStorage' in global
-  };
-
-  var getCache = function getCache(name, version) {
-    var result = {
-      name: name,
-      version: version,
-      code: ''
-    };
-
-    try {
-      var str = global.localStorage.getItem(name);
-      if (str) {
-        var obj = JSON.parse(str);
-        if (obj.version === version) result = obj;
-      }
-    } catch (err) {
-      console.error(err);
-    }
-
-    return result;
-  };
-
-  var setCache = function setCache(opts) {
-    try {
-      global.localStorage.setItem(opts.name, JSON.stringify(opts));
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  var encode = function encode(s) {
+  var encode = exports.encode = function encode(s) {
     return String(s).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/ /g, '&nbsp;').replace(/\'/g, '&#39;').replace(/\"/g, '&quot;').replace(/\n/g, '<br>');
   };
 
-  var decode = function decode(s) {
+  var decode = exports.decode = function decode(s) {
     return String(s).replace(/&amp;/g, '&').replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&nbsp;/g, ' ').replace(/&#39;/g, '\'').replace(/&quot;/g, '"').replace(/<br>/g, '\n');
   };
 
-  var defaultParseOpts = { cache: true, name: '', version: '0' };
+  var compile = exports.compile = function compile() {
+    var tpl = arguments.length <= 0 || arguments[0] === undefined ? '' : arguments[0];
+    var reg = /\<\#([\s\S]+?)\#\>/g;
+    var cursor = 0;
+    var code = '';
+    var match = undefined;
 
-  var parse = function parse() {
+    var push = function push(line, js) {
+      if (!line) return push;
+
+      if (js) {
+        var _s = line.charAt(0);
+
+        switch (_s) {
+          case '=':
+            code += '\n            __r__.push(encode(' + line.substr(1) + '));\n          ';
+            break;
+
+          case '-':
+            code += '\n            __r__.push(' + line.substr(1) + ');\n          ';
+            break;
+
+          default:
+            code += line;
+            break;
+        }
+      } else {
+        code += '\n        __r__.push("' + line.replace(/"/g, '\\"') + '");\n      ';
+      }
+
+      return push;
+    };
+
+    while (match = reg.exec(tpl)) {
+      push(tpl.slice(cursor, match.index))(match[1], true);
+      cursor = match.index + match[0].length;
+    }
+
+    push(tpl.substr(cursor, tpl.length - cursor));
+    code = ('with(obj) {\n            var __r__ = [];\n            ' + code + '\n            return __r__.join("");\n          }').replace(/[\r\t\n]/g, '');
+    return function (data) {
+      return new Function('obj, encode', code).apply(data, [data, encode]);
+    };
+  };
+
+  var parse = exports.parse = function parse() {
     var tpl = arguments.length <= 0 || arguments[0] === undefined ? '' : arguments[0];
     var data = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
-    var opts = arguments.length <= 2 || arguments[2] === undefined ? defaultParseOpts : arguments[2];
-
-    var reg = /\<\#([\s\S]+?)\#\>/g,
-        code = '',
-        cursor = 0,
-        result = undefined,
-        match = undefined,
-        cacheCode = '',
-        cacheObj = {};
-
-    var isCache = support.localStorage && opts.cache && opts.name;
-
-    if (isCache) {
-      cacheObj = getCache(opts.name, opts.version);
-      if (cacheObj.code) cacheCode = cacheObj.code;
-    }
-
-    if (!cacheCode) {
-      (function () {
-        var push = function push(line, js) {
-          if (!line) return push;
-          if (js) {
-            var _s = line.charAt(0);
-
-            switch (_s) {
-              case '=':
-                code += '\n              __r__.push(encode(' + line.substr(1) + '));\n            ';
-                break;
-              case '-':
-                code += '\n              __r__.push(' + line.substr(1) + ');\n            ';
-                break;
-              default:
-                code += line;
-                break;
-            }
-          } else {
-            code += '\n          __r__.push("' + line.replace(/"/g, '\\"') + '");\n        ';
-          }
-
-          return push;
-        };
-
-        while (match = reg.exec(tpl)) {
-          push(tpl.slice(cursor, match.index))(match[1], true);
-          cursor = match.index + match[0].length;
-        }
-
-        push(tpl.substr(cursor, tpl.length - cursor));
-        code = ('with(obj) {\n              var __r__ = [];\n              ' + code + '\n              return __r__.join("");\n            }').replace(/[\r\t\n]/g, '');
-
-        if (isCache) setCache({ name: opts.name, version: opts.version, code: code });
-      })();
-    } else {
-      code = cacheCode;
-    }
-
-    try {
-      result = new Function('obj', 'encode', code).apply(data, [data, encode]);
-    } catch (e) {
-      console.error('\'' + e.message + '\'', 'in \n\n Code: \n', code, '\n');
-    }
-
-    return result;
-  };
-
-  module.exports = {
-    parse: parse,
-    decode: decode,
-    encode: encode
+    return compile(tpl)(data);
   };
 });
-
-//# sourceMappingURL=tmpl.js.map
---
+++ test/index.js
@@ -0,0 +1,71 @@
+var should = require('should');
+var tmpl = require('../build/tmpl');
+console.log(tmpl);
+
+var t1 = '<h1><#= title #></h1>';
+var maps = [
+  { from: '<', to: '&lt;' },
+  { from: '>', to: '&gt;' },
+  { from: ' ', to: '&nbsp;' },
+  { from: '"', to: '&quot;' },
+  { from: '\'', to: '&#39;' },
+  { from: '&', to: '&amp;' },
+];
+
+describe('compile', function() {
+  it('compile should be function', function() {
+    tmpl.compile(t1).should.be.type('function');
+  });
+
+  it('compile return function should can parse template', function() {
+    tmpl.compile(t1)({title: 'haha'}).should.be.equal('<h1>haha</h1>');
+  });
+
+  it('compile should parse many time', function() {
+    var compile = tmpl.compile(t1);
+    compile({title: 'haha'}).should.be.equal('<h1>haha</h1>');
+    compile({title: 'nimei'}).should.be.equal('<h1>nimei</h1>');
+  });
+});
+
+describe('parse', function() {
+  it('parse should be compile template to string', function() {
+    tmpl.parse(t1, {title: 'haha'}).should.be.equal('<h1>haha</h1>');
+  });
+
+  it('<#= #> must escape html tag', function() {
+    tmpl.parse(t1, {title: '<script>haha</script>'}).should.be
+      .equal('<h1>&lt;script&gt;haha&lt;/script&gt;</h1>');
+  });
+
+  it('<#= #> must did\' show html tag', function() {
+    tmpl.parse(t1, {title: '<script>haha</script>'}).should.not
+      .equal('<h1><script>haha</script></h1>');
+  });
+
+  it('<#- #> don\'t escape html tag', function() {
+    tmpl.parse(t1, {title: '<script>haha</script>'}).should.not
+      .equal('<h1><script>haha</script></h1>');
+  });
+
+  it('<# #> can be declare variable', function() {
+    tmpl.parse('<# var a = 1; #><h1><#= a #></h1>').should.be
+      .equal('<h1>1</h1>');
+  });
+});
+
+describe('encode', function() {
+  maps.map(function(d) {
+    it('encode ' + d.from + ' to ' + d.to, function() {
+      tmpl.encode(d.from).should.be.equal(d.to);
+    });
+  });
+});
+
+describe('decode', function() {
+  maps.map(function(d) {
+    it('decode ' + d.to + ' to ' + d.from, function() {
+      tmpl.decode(d.to).should.be.equal(d.from);
+    });
+  });
+});
