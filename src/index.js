75a444d6f13d2e1e93756a5250591ce8450db228
CoffeeDeveloper <coffeedeveloper@gmail.com>
2017-03-19 16:01:22 +0800

    修改打包方式，增加压缩版本

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-(function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-	typeof define === 'function' && define.amd ? define(['exports'], factory) :
-	(factory((global.Tmpl = global.Tmpl || {})));
-}(this, (function (exports) { 'use strict';
-
 /** @module coffee-tmpl */
 
 /**
@@ -12,13 +6,13 @@
  * @param {string} s - 需要做html encode的字符串
  * @return {string} html encode之后的字符串
  */
-var encode = function (s) { return String(s).replace(/&/g, '&amp;')
+export const encode = s => String(s).replace(/&/g, '&amp;')
                                     .replace(/</g, '&lt;')
                                     .replace(/>/g, '&gt;')
                                     .replace(/ /g, '&nbsp;')
                                     .replace(/\'/g, '&#39;')
                                     .replace(/\"/g, '&quot;')
-                                    .replace(/\n/g, '<br>'); };
+                                    .replace(/\n/g, '<br>');
 
 /**
  * 将HTML特殊字符转回成原字符
@@ -26,15 +20,15 @@ var encode = function (s) { return String(s).replace(/&/g, '&amp;')
  * @param {string} s - 需要做html decode的字符串
  * @return {string} html decode之后的字符串
  */
-var decode = function (s) { return String(s).replace(/&amp;/g, '&')
+export const decode = s => String(s).replace(/&amp;/g, '&')
                                     .replace(/&lt;/g, '<')
                                     .replace(/&gt;/g, '>')
                                     .replace(/&nbsp;/g, ' ')
                                     .replace(/&#39;/g, '\'')
                                     .replace(/&quot;/g, '"')
-                                    .replace(/<br>/g, '\n'); };
+                                    .replace(/<br>/g, '\n');
 
-var opts = {
+let opts = {
   delimiter: '%',
 };
 
@@ -44,7 +38,7 @@ var opts = {
  * @param {string} key - 设置项
  * @param {any} val - 设置内容
  */
-var set = function (key, val) {
+export const set = (key, val) => {
   opts[key] = val;
 };
 
@@ -54,33 +48,37 @@ var set = function (key, val) {
  * @param {string} tpl - 模板字符串
  * @return {function} 编译后的可执行渲染的函数，主要用于缓存执行，提高效率
  */
-var compile = function (tpl) {
-  if ( tpl === void 0 ) tpl = '';
+export const compile = (tpl = '') => {
+  let reg = new RegExp(`\\<\\${opts.delimiter}([\\s\\S]+?)\\${opts.delimiter}\\>`, 'g');
+  let cursor = 0;
+  let code = '';
+  let match;
 
-  var reg = new RegExp(("\\<\\" + (opts.delimiter) + "([\\s\\S]+?)\\" + (opts.delimiter) + "\\>"), 'g');
-  var cursor = 0;
-  var code = '';
-  var match;
-
-  var push = function (line, js) {
-    if (!line) { return push; }
+  const push = (line, js) => {
+    if (!line) return push;
 
     if (js) {
-      var _s = line.charAt(0);
+      let _s = line.charAt(0);
 
       switch (_s) {
         case '=':
-          code += "\n            __r__.push(encode(" + (line.substr(1)) + "));\n          ";
+          code += `
+            __r__.push(encode(${line.substr(1)}));
+          `;
           break;
         case '-':
-          code += "\n            __r__.push(" + (line.substr(1)) + ");\n          ";
+          code += `
+            __r__.push(${line.substr(1)});
+          `;
           break;
         default:
           code += line;
           break;
       }
     } else {
-      code += "\n        __r__.push(\"" + (line.replace(/"/g, '\\"')) + "\");\n      ";
+      code += `
+        __r__.push("${line.replace(/"/g, '\\"')}");
+      `;
     }
 
     return push;
@@ -92,13 +90,13 @@ var compile = function (tpl) {
   }
 
   push(tpl.substr(cursor, tpl.length - cursor));
-  code = (("with(obj) {\n            var __r__ = [];\n            " + code + "\n            return __r__.join(\"\");\n          }")).replace(/[\r\t\n]/g, '');
+  code = (`with(obj) {
+            var __r__ = [];
+            ${code}
+            return __r__.join("");
+          }`).replace(/[\r\t\n]/g, '');
 
-  return function (data) {
-    if ( data === void 0 ) data = {};
-
-    return new Function('obj, encode', code).apply(data, [data, encode]);
-  };
+  return (data = {}) => new Function('obj, encode', code).apply(data, [data, encode]);
 };
 
 /**
@@ -108,19 +106,4 @@ var compile = function (tpl) {
  * @param {object} data - 模板的数据源
  * @return {string} 编译后的模板字符串
  */
-var parse = function (tpl, data) {
-  if ( tpl === void 0 ) tpl = '';
-  if ( data === void 0 ) data = {};
-
-  return compile(tpl)(data);
-};
-
-exports.encode = encode;
-exports.decode = decode;
-exports.set = set;
-exports.compile = compile;
-exports.parse = parse;
-
-Object.defineProperty(exports, '__esModule', { value: true });
-
-})));
+export const parse = (tpl = '', data = {}) => compile(tpl)(data);
